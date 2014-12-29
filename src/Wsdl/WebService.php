c646b05c2a23538356e5dd7e8f67b824a37a2bcb
Jindrich Pilar <pilar.jindrich@gmail.com>
2014-12-29 08:46:03 -0500

    WebService refactored

--- src/Wsdl/WebService.php
+++ src/Wsdl/WebService.php
@@ -37,7 +37,8 @@ class WebService extends SoapClient implements EventDispatcherInterface
      * @param array $init Zakladni informace pro vsechny pozadavky
      * @param bool $compression Ma pouzivat kompresi na prenasena data?
      */
-    public function __construct($wsdl, array $soapOpts) {
+    public function __construct($wsdl, array $soapOpts)
+    {
         $this->init = $soapOpts;
         if (empty($wsdl)) {
             throw new AbortException("WSDL musí být nastaven");
@@ -47,8 +48,14 @@ class WebService extends SoapClient implements EventDispatcherInterface
 
     /**
      * Magicka metoda starjici se spravne volani SOAP metod
+     *
+     * @param string $function_name Jmeno funkce volane na objektu
+     * @param array  $arguments     Argumenty funkce volane na objektu
+     *
+     * @return mixed
      */
-    public function __call($function_name, $arguments) {
+    public function __call($function_name, $arguments)
+    {
 
         return $this->__soapCall($function_name, $arguments);
     }
@@ -59,55 +66,28 @@ class WebService extends SoapClient implements EventDispatcherInterface
      * @param string $function_name
      * @param array $arguments ([0]=args [1]=cover)
      *
-     * @return type
+     * @return mixed
      */
-    public function __soapCall($function_name, $arguments, $options = null, $input_headers = null, &$output_headers = null) {
-        //public function __call($function_name, $arguments) {
+    public function __soapCall($function_name, $arguments, $options = null, $input_headers = null, &$output_headers = null)
+    {
         $fname = ucfirst($function_name);
-
-        if (!isset($arguments[0]) || !is_array($arguments[0])) {
-            $arguments[0] = array();
-        }
-
-        $args = array_merge($this->init, $arguments[0]); //k argumentum připoji vlastni informace o aplikaci a uzivateli
-        //cover
-        if (isset($arguments[1]) && $arguments[1] !== null) {//pokud je zadan druhy parametr tak lze prejmenovat obal dat
-            $matches = preg_split('~/~', $arguments[1]); //rozdeli to na stringy podle /
-            $matches = array_reverse($matches); //pole se budou vytvaret zevnitr ven
-
-            $matches[] = 0; //zakladni obal 0=>...
-
-            foreach ($matches as $value) {
-                $args = array($value => $args);
-            }
-        } else {
-            $function_name = strtolower(substr($function_name, 0, 1)) . substr($function_name, 1); //nahrazuje lcfirst
-            $args = array(array($function_name . "Input" => $args));
-        }
+        $args = $this->prepareArgs($fname, $arguments);
 
         if ($this->hasListeners()) {
             $query = new SkautisQuery($fname, $args, debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS));
         }
-        try {
-            $ret = parent::__soapCall($fname, $args);
 
-            //pokud obsahuje Output tak vždy vrací pole i s jedním prvkem.
-            if (isset($ret->{$fname . "Result"})) {
-                if (isset($ret->{$fname . "Result"}->{$fname . "Output"})) {
-                    if ($ret->{$fname . "Result"}->{$fname . "Output"} instanceof stdClass) { //vraci pouze jednu hodnotu misto pole?
-                        $ret = array($ret->{$fname . "Result"}->{$fname . "Output"}); //vraci pole se stdClass
-                    } else {
-                        $ret = $ret->{$fname . "Result"}->{$fname . "Output"}; //vraci pole se stdClass
-                    }
-                } else {
-                    $ret = $ret->{$fname . "Result"}; //neobsahuje $fname.Output
-                }
-            }
+        try {
+            $soapResponse = parent::__soapCall($fname, $args);
+
+            $soapResponse = $this->parseOutput($fname, $soapResponse);
+
             if ($this->hasListeners()) {
-                $this->dispatch(self::EVENT_SUCCESS, $query->done($ret));
+                $this->dispatch(self::EVENT_SUCCESS, $query->done($soapResponse));
             }
-            return $ret; //neobsahuje $fname.Result
-        } catch (SoapFault $e) {
+            return $soapResponse;
+        }
+        catch (SoapFault $e) {
             if ($this->hasListeners()) {
                 $this->dispatch(self::EVENT_FAILURE, $query->done(NULL, $e));
             }
@@ -119,6 +99,67 @@ class WebService extends SoapClient implements EventDispatcherInterface
             }
             throw new WsdlException($e->getMessage());
         }
+    }
+
+    /**
+     * Z defaultnich parametru a parametru callu vytvori argumenty pro SoapClient::__soapCall
+     *
+     * @param string $function_name Jmeno funkce volane pres SOAP
+     * @param array $arguments      Argumenty k mergnuti s defaultnimy
+     *
+     * @return array Argumenty pro SoapClient::__soapCall
+     */
+    protected function prepareArgs($function_name, array $arguments)
+    {
+        if (!isset($arguments[0]) || !is_array($arguments[0])) {
+            $arguments[0] = [];
+        }
+
+        $args = array_merge($this->init, $arguments[0]); //k argumentum připoji vlastni informace o aplikaci a uzivateli
+
+        if (!isset($arguments[1]) || $arguments[1] === null) {
+            $function_name = strtolower(substr($function_name, 0, 1)) . substr($function_name, 1); //nahrazuje lcfirst
+            $args =[[$function_name . "Input" => $args]];
+            return $args;
+        }
+
+        //pokud je zadan druhy parametr tak lze prejmenovat obal dat
+        $matches = preg_split('~/~', $arguments[1]); //rozdeli to na stringy podle /
+        $matches = array_reverse($matches); //pole se budou vytvaret zevnitr ven
+
+        $matches[] = 0; //zakladni obal 0=>...
+
+        foreach ($matches as $value) {
+            $args = [$value => $args];
+        }
+
+        return $args;
+    }
+
+    /**
+     * Parsuje output ze SoapClient do jednotného formátu
+     *
+     * @param string $fname Jméno funkce volané přes SOAP
+     * @param mixed $ret    Odpoveď ze SoapClient::__soapCall
+     *
+     * @return array
+     */
+    protected function parseOutput($fname, $ret)
+    {
+        //pokud obsahuje Output tak vždy vrací pole i s jedním prvkem.
+        if (!isset($ret->{$fname . "Result"})) {
+            return $ret;
+        }
+
+        if (!isset($ret->{$fname . "Result"}->{$fname . "Output"})) {
+            return $ret->{$fname . "Result"}; //neobsahuje $fname.Output
+        }
+
+        if ($ret->{$fname . "Result"}->{$fname . "Output"} instanceof stdClass) { //vraci pouze jednu hodnotu misto pole?
+            return [$ret->{$fname . "Result"}->{$fname . "Output"}]; //vraci pole se stdClass
+        }
+
+        return $ret = $ret->{$fname . "Result"}->{$fname . "Output"}; //vraci pole se stdClass
     }
 
 }
