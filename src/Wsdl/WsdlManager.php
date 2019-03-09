c721b267f4e322d8ac3d36edd280a1f9a88ffe96
František Hána <sinacek@gmail.com>
2019-03-09 14:28:24 +0100

    check $headers for false value (#74)

--- src/Wsdl/WsdlManager.php
+++ src/Wsdl/WsdlManager.php
@@ -173,7 +173,7 @@ class WsdlManager
     public function isMaintenance(): bool
     {
         $headers = get_headers($this->getWebServiceUrl("UserManagement"));
-        return !in_array('HTTP/1.1 200 OK', $headers);
+        return !$headers || !in_array('HTTP/1.1 200 OK', $headers);
     }
 
     /**
