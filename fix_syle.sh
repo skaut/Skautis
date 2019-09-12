e477bff41093355b63b66129f3b09af35171edc3
Jindrich Pilar <pilar.jindrich@gmail.com>
2019-09-12 16:01:26 +0200

    PHPStan analyza v pre-commit hooku

--- fix_syle.sh
+++ fix_syle.sh
@@ -4,6 +4,7 @@ if [ ! -f ./vendor/bin/php-cs-fixer ]; then
     composer update --dev; 
 fi
 
+./vendor/bin/phpstan analyze  -c phpstan.neon --level 7 --no-progress  src
 
 #Fixni pouze nove a upravene soubory
 for file in $(git diff --name-only); do
